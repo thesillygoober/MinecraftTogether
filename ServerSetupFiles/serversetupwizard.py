f8475b8f9a6dcd4d02b9e6428e5818511676cc32
thesillygoober <70186143+thesillygoober@users.noreply.github.com>
2023-06-24 20:03:14 -0400

    Little bit of tkinter stuff for ssw.py
    doing this because vscode on my laptop is being annoying and not detecting my modules so i'll just do stuff on my pc for now

--- ServerSetupFiles/serversetupwizard.py
+++ ServerSetupFiles/serversetupwizard.py
@@ -4,6 +4,17 @@
 # Imports
 from serverfilelinks import get_links
 from serverdownloader import download_server_file
+import tkinter as tk
+import customtkinter as ctk
+
+# Setup
+ctk.set_appearance_mode("system")  # Modes: system (default), light, dark
+ctk.set_default_color_theme("green") 
+
+app = ctk.CTk()
+app.title("MCT Server Setup")
+app.resizable(False, False)
+app.geometry("200x500")
 
 # Variables
 paper_links = {}
@@ -12,3 +23,6 @@ fabric_links = {}
 
 # Main
 paper_links, forge_links, fabric_links = get_links()
+
+# End
+app.mainloop()
